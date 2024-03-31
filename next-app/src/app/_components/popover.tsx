d3ae440914a6325b31acce303ece6f07cdd5494c
Yash Gunjal <yashgunjal98@gmail.com>
2024-03-30 22:25:46 -0400

    add Navbar

--- next-app/src/app/_components/popover.tsx
+++ next-app/src/app/_components/popover.tsx
@@ -12,8 +12,20 @@ interface PopoverProps {
 const Popover: React.FC<PopoverProps> = ({ buttonRef, children , isVisible,  setIsVisible }) => {
   const popoverRef = useRef<HTMLDivElement | null>(null);
 
+  console.log("State", isVisible)
+
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
+
+    const handleClickOutside1 = (event: MouseEvent | TouchEvent) => {
+        const targetElement = event.target as HTMLElement; // Cast to HTMLElement to access .closest method
+    
+        if (buttonRef.current && !buttonRef.current.contains(targetElement) &&
+            !targetElement.closest('#user-dropdown')) {
+                setIsVisible();
+        }
+      };
+    
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
       if (
         buttonRef.current && !buttonRef.current.contains(event.target as Node) &&
         popoverRef.current && !popoverRef.current.contains(event.target as Node)
@@ -21,10 +33,14 @@ const Popover: React.FC<PopoverProps> = ({ buttonRef, children , isVisible,  set
         setIsVisible();
       }
     };
-
+    
     document.addEventListener('mousedown', handleClickOutside);
-    return () => document.removeEventListener('mousedown', handleClickOutside);
-  }, [buttonRef]);
+    document.addEventListener('touchstart', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
+    };
+  }, [buttonRef, isVisible]);
 
   return (
     <>
@@ -32,7 +48,7 @@ const Popover: React.FC<PopoverProps> = ({ buttonRef, children , isVisible,  set
         <div
           ref={popoverRef}
           className="absolute z-50 my-4 w-48 list-none divide-y divide-gray-100 rounded-lg bg-white text-base shadow dark:divide-gray-600 dark:bg-gray-700"
-          style={{ top: '20%', right: 0 }}
+          style={{ top: '80%', right: 0 }}
         >
           {children}
         </div>
--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,18 +1,11 @@
-import { SignInButton, SignOutButton, auth } from "@clerk/nextjs";
-
-import { CreatePost } from "@/app/_components/create-post";
-import { api } from "@/trpc/server";
 import Link from "next/link";
 
 export default async function Home() {
   // const hello = await api.post.hello({ text: "from tRPC" });
-  const { userId } = auth();
-
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        {userId ? <SignOutButton /> : <SignInButton />}
         <div>
           <Link href="/chat" > 
           <button className="bg-purple-700 border-spacing-2 p-4 rounded-md">{"Lets Chat"}</button>
